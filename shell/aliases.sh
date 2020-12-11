f6eef60b9c30b5fbf898210d80bc8f73b20c0a04
Lucas Gonzalez <lucasgonzalez.dev@gmail.com>
2020-12-11 14:08:52 -0300

    psuh

---
+++ shell/aliases.sh
@@ -0,0 +1,38 @@
+# Enable aliases to be sudo’ed
+alias sudo='sudo '
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ll="ls -l"
+alias la="ls -la"
+alias ~="cd ~"
+
+# Git
+alias gaa="git add -A"
+alias gc="dot git commit"
+alias gca="git add --all && git commit --amend --no-edit"
+alias gco="git checkout"
+alias gcb="git checkout -b"
+alias gd="dot git pretty-diff"
+alias gs="git status -sb"
+alias gf="git fetch --all -p"
+alias gps="git push"
+alias gpsf="git push --force"
+alias gpl="git pull --rebase --autostash"
+alias gb="git branch"
+alias gl="dot git pretty-log"
+
+# Utils
+alias k='kill -9'
+alias i.='(idea $PWD &>/dev/null &)'
+alias c.='(code $PWD &>/dev/null &)'
+alias o.='open .'
+alias v=openNvim
+
+function openNvim {
+  if [ $# -eq 0 ]; then
+    nvim ./
+  else
+    nvim $1
+  fi
+}
